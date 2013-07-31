10a67627d013a7255e6cdc82c92158fd31f13c65
nullproduction <mezhevikin@gmail.com>
2013-07-31 12:14:25 +0400

    Initial Commit

---
+++ MCCoreData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MCCoreData
+//
+//  Created by Администратор on 7/31/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
